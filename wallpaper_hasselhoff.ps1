c1d8fba8aeec8f3b7f22ba7c5cf2b37919ab0149
MightyIT <m_hoffmann83@outlook.de>
2022-11-23 11:21:42 +0100

    Create wallpaper_hasselhoff.ps1

---
+++ wallpaper_hasselhoff.ps1
@@ -0,0 +1,39 @@
+Function Set-WallPaper($Image) {
+<#
+ 
+    .SYNOPSIS
+    Applies a specified wallpaper to the current user's desktop
+    
+    .PARAMETER Image
+    Provide the exact path to the image
+  
+    .EXAMPLE
+    Set-WallPaper -Image "C:\Wallpaper\Default.jpg"
+  
+#>
+  
+Add-Type -TypeDefinition @" 
+using System; 
+using System.Runtime.InteropServices;
+  
+public class Params
+{ 
+    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
+    public static extern int SystemParametersInfo (Int32 uAction, 
+                                                   Int32 uParam, 
+                                                   String lpvParam, 
+                                                   Int32 fuWinIni);
+}
+"@ 
+  
+    $SPI_SETDESKWALLPAPER = 0x0014
+    $UpdateIniFile = 0x01
+    $SendChangeEvent = 0x02
+  
+    $fWinIni = $UpdateIniFile -bor $SendChangeEvent
+  
+    $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
+ 
+}
+iwr https://wallpapercave.com/wp/wp3805150.jpg -OutFile hasselhoff.jpg
+Set-WallPaper -Image hasselhoff.jpg
