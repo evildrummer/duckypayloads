c90c67ea39b30020e9b443b916ce851b54799b91
MightyIT <m_hoffmann83@outlook.de>
2022-11-23 11:40:34 +0100

    Update wallpaper_hasselhoff.ps1

--- wallpaper_hasselhoff.ps1
+++ wallpaper_hasselhoff.ps1
@@ -1,30 +1,9 @@
 iwr https://wallpapercave.com/wp/wp3805150.jpg -OutFile hasselhoff.jpg
 
-
-<#
-Credits/ info from https://stackoverflow.com/questions/19989906/how-to-set-wallpaper-style-fill-stretch-according-to-windows-version
- ' Set the wallpaper style and tile.
-        ' Two registry values are set in the Control Panel\Desktop key.
-        ' TileWallpaper
-        '  0: The wallpaper picture should not be tiled
-        '  1: The wallpaper picture should be tiled
-        ' WallpaperStyle
-        '  0:  The image is centered if TileWallpaper=0 or tiled if TileWallpaper=1
-        '  2:  The image is stretched to fill the screen
-        '  6:  The image is resized to fit the screen while maintaining the aspect
-        '      ratio. (Windows 7 and later)
-        '  10: The image is resized and cropped to fill the screen while
-        '      maintaining the aspect ratio. (Windows 7 and later)
-        '  22: Span image
-#>
-
-# Pass Parameters to the Script
-
-Function SetWallpaper(){
 [CmdletBinding()]
 param(
 [Parameter()]
-[string]$Path,
+#[string]$Path,
 [ValidateSet('Centered', 'Stretched', 'Fill', 'Fit', 'Span')] $Style = 'Fill',
 [ValidateSet('Tiles','NoTiles')] $Tiled = '0')
 #---------------------------------------------------#
@@ -76,7 +55,5 @@ Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name wallpaperstyle -Value
 Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name TileWallpaper -Value $WTile[$Tiled]
 
 #Apply the Change on the system
-[Win32.Wallpaper]::SetWallpaper($Path)    
-SetWallpaper($MyWallpaper)
-}
-SetWallpaper -Path "hasselhoff.jpg" -Style Fit
+$Path="hasselhoff.jpg"
+[Win32.Wallpaper]::SetWallpaper($Path)
